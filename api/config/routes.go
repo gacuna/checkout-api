f0be3fa3f41caf849680bab49106dda8a987dcfc
Gustavo Acuna <gustavo.acuna-contractro@fabfitfun.com>
2023-12-11 09:40:05 -0300

    add endpoint

--- api/config/routes.go
+++ api/config/routes.go
@@ -8,6 +8,7 @@ func init() {
 	v1 := engine.Group("/v1")
 	{
 		v1.GET("/test", controllers.GetTest)
+		v1.GET("/checkout", controllers.GetTest)
 	}
 	engine.GET("/health/check", controllers.HealthCheck)
 }
---
+++ model/CheckoutPage.go
@@ -0,0 +1,24 @@
+package model
+
+type CheckoutPage struct {
+	/*
+		public Guid ShopId { get; set; }
+	    public Guid? PaymentId { get; set; }
+	    public Guid CheckoutPageId { get; set; }
+	    public string CheckoutPageCode { get; set; }
+	    public DateTime CreatedAt { get; set; }
+	    public string Description { get; set; }
+	    public string CurrencyCode { get; set; }
+	    public decimal Amount { get; set; }
+	    public string LanguageCode { get; set; }
+	    public string OrderId { get; set; }
+	    public string CustomerData { get; set; }
+	    public CustomerQueryModel Customer => JsonConvert.DeserializeObject<CustomerQueryModel>(CustomerData);
+	    public string NotificationUrl { get; set; }
+	    public string ReturnUrl { get; set; }
+	    public string CancelUrl { get; set; }
+	    public string ApiKey { get; set; }
+	    public string SecretKey { get; set; }
+	    public bool HideCompletedFields { get; set; }
+	*/
+}
